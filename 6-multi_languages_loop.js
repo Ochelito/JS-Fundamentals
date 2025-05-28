0e826705e5b77412a1081a458e5f9d62c5ec2ced
Idoko Augustine <linkwithaugustine@gmail.com>
2025-05-28 08:06:23 -0700

    added 6-multi_languages_loop.js file

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
+
+//Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
