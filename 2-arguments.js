6412713f55f2ac27c70f08a8fff78e3147c3bdc0
Idoko Augustine <linkwithaugustine@gmail.com>
2025-05-28 07:37:05 -0700

    added 3-value_argument.js file

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,13 @@
 const args = process.argv.slice(2);
 
-if (args.length === 0) {
+if (args.length === 0) { 
 console.log("No argument");
 } else if (args.length === 1){
     console.log("Argument found");
 } else {
     console.log("Arguments found");
 }
+// alternate to using .length is if (args[0] === undefined) {
+ // console.log("No argument");
+//} else {
+ // console.log(args[0]);
